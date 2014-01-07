4ddb61e228f29f90f0fe7a9467dca359f7ef33f7
freestyle21 <675861708@qq.com>
2014-01-06 20:04:29 -0800

    reg1 &reg page

---
+++ js/form-operate.js
@@ -0,0 +1,158 @@
+define(function(require, exports, module) {
+
+  var $ = require('jquery');
+  var validation = require('validation');
+  var Dialog = require('reg-dialog');
+
+  function FormOperation(options) {
+    this.options = options || {};
+  }
+
+  module.exports = FormOperation;
+
+  FormOperation.prototype = {
+    validation : function() {
+          $.each(this.options.forNums, function( no, forNum) {
+              forNum.validation(
+                  {rule:'numeric' , required:false} ,  '格式错误,只能是数字' );
+          });
+          $.each(this.options.forEmails, function( no, forEmail) {
+              forEmail.validation(
+                  {rule:'email' , required:false} ,  '邮箱格式错误' );
+          });
+    },
+    showOtherInput: function() {
+        $('.events-in-year').change(function() {
+            if( $(this).val() === '03' ) {
+                $(this).siblings('input').show();
+            } else {
+                $(this).siblings('input').hide();
+            }
+        });
+    },
+    showDialog: function() {
+        $('.illness-info-add').click(function() {
+            var parent = $(this).parents('tr')[0];
+            
+            var columns = [
+                {
+                  'name': '病名',
+                  'needTime': false
+                },
+                {
+                  'name': '发病时间',
+                  'needTime': true
+                },
+                {
+                  'name': '确诊时间',
+                  'needTime': true
+                },
+                {
+                  'name': '痊愈时间',
+                  'needTime': true
+                },
+                {
+                  'name': '后遗诊',
+                  'needTime': false
+                }
+              ]
+            var dialog = new Dialog('疾病信息', columns, $(parent));
+            dialog.render();
+            return false;
+        });
+
+        $('.family-illness-add').click(function() {
+            var parent = $(this).parents('tr')[0];
+            
+            var columns = [
+                {
+                  'name': '与本人关系',
+                  'needTime': false
+                },
+                {
+                  'name': '家族病',
+                  'needTime': false
+                }
+              ]
+            var dialog = new Dialog('家族病史', columns, $(parent));
+            dialog.render();
+            return false;
+        });
+
+        $('.surgery-add').click(function() {
+            var parent = $(this).parents('tr')[0];
+            
+            var columns = [
+                {
+                  'name': '名称',
+                  'needTime': false
+                },
+                {
+                  'name': '手术时间',
+                  'needTime': true
+                },
+                {
+                  'name': '痊愈时间',
+                  'needTime': true
+                },
+                {
+                  'name': '后遗症',
+                  'needTime': false
+                }
+
+              ]
+            var dialog = new Dialog('手术史', columns, $(parent));
+            dialog.render();
+            return false;
+        });
+    },
+    checkBeforeSubmit: function() {
+        $('#reg-confirm-psw').blur(function() {
+            if( $('#reg-confirm-psw').val() !== $('#reg-password').val() ) {
+                $(this).parent('div').append($('<span class="err">两次密码不一致</span>'));
+            } else {
+                $(this).parent('div').find('.err').remove();
+            }
+        });
+        $('.reg-submit').click(function() {
+            var notNull = ['#reg-email', '#reg-password', '#reg-confirm-psw', '#birthday', '#loc_province', '#loc_city', '#degree', '#major', '#favor'];
+            if( !noNull(notNull) ) {
+                $('.reg-submit-error').show();
+                return false;
+            } 
+            if( $('.err').length !== 0) {
+                // validation 没有通过。
+                $('.reg-submit-error').show();
+                return false;
+            }
+        });
+
+
+        $('.reg1-submit').click(function() {
+            
+            var notNull = ['#reg-height', '#reg-weight', '#reg-age', '#reg-addr', '#bear-condition', '.reg-career', '.sleep-condition', '#ill-bear'];
+            if( !noNull(notNull) ) {
+                $('.reg1-submit-error').show();
+                return false;
+            } 
+            if( $('.err').length !== 0) {
+                // validation 没有通过。
+                $('.reg1-submit-error').show();
+                return false;
+            }
+        });
+        function noNull(objs) {
+            var result = true;
+            $.each(objs, function( no, obj ) {
+                // 必须每个都不能为空
+                if( $(obj).val() === '') {
+                    
+                    result = false;
+                }
+            });
+            return result;
+        }
+    }
+  }
+});
+
--- js/reg.js
+++ js/reg.js
@@ -1,29 +1,39 @@
 define(function(require) {
 	var $ = require('jquery');
 	var showLocation = require('area');
-	var validation = require('validation');
-	var util = require('util');
+	
+	
+	var formOperation = require('form-operate');
 	require('datepicker');
 
 
 	(function($) {
-		var jquery_plugin_css = ['../css/lib/datepicker.css', '../css/lib/font-awesome/css/font-awesome.css'];
-		$.each(jquery_plugin_css, function(no, css) {
-			util.loadDateCss(css);	
-		});
+		
 		// 地理位置
 		showLocation();
-		checkForm();
-		// 表单验证&组件
-		function checkForm () {
-			$('.reg-date-picker').datepicker({
-		        format: 'mm-dd-yyyy'
-		    });
-		    $( '#reg-email' ).validation(
-		        {rule:'email' , required:false} ,  '邮箱格式错误.' );
-		    $( '#reg-height' ).validation(
-		        {rule:'numeric' , required:false} ,  '格式错误,只能是数字' );
+
+		// 动态加载datepicker需要的css
+		var util = require('util');
+		util.loadDateCss();
+
+		// 表单验证
+		var opt = {
+			forNums: [$( '#reg-height' ), $( '#reg-weight' ), $( '#reg-waistline' ), $( '#reg-arm' ), $( '#reg-age' )],
+			forEmails: [$( '#reg-email' )]
 		}
+		var regForm = new formOperation(opt);
+		regForm.validation();
+		regForm.showOtherInput();
+		regForm.showDialog();
+		regForm.checkBeforeSubmit();
+
+		// datepicker
+		$('.reg-date-picker').datepicker({
+		    format: 'mm-dd-yyyy'
+		});
+		
 	})($);
+
+	
 });
 
