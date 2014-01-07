f95026641426e3a27d9ba5ff49ce7150c7247740
winterJIE <124400122@qq.com>
2014-01-07 14:52:16 +0800

    add physiqueMonitoring

---
+++ js/physique/physiqueMonitoring.js
@@ -0,0 +1,62 @@
+/**
+ * Created by winter on 14-1-7.
+ */
+define(function(require){
+
+    var $ = require("jquery");
+    require ('jquery.easing');
+
+    var result = {};
+    var table = $(".sixtyQuestion").find("table");
+    table.find("input[type = 'radio']").attr("disabled","disabled");
+    $(".sexAge").find("button").on("click",function(){
+        var formTestResult = formTest();
+        if( false === formTestResult ) {
+            var target = $("div.sexAge").find("div.age");
+            target.addClass("has-error");
+            target.find("input[type = 'text']").attr("id","inputError");
+        } else {
+            result.sexAgeRagion = getSexAgeRegion();
+            $(".sexAge").slideUp();
+            var firstTr = table.find("tr:eq(0) input");
+            firstTr.removeAttr("disabled");
+            table.find("tr:eq(0)").addClass("trbg");
+//            var firstInputvalue = table.find("input[name = '1'][checked]").val();
+
+        }
+    });
+
+    function radioCheck() {
+        var radio = table.find("tr");
+        var radioNum = radio.length;
+
+    }
+    radioCheck();
+
+    function formTest(){
+        var age = $(".sexAge").find("input[type = 'text']").val();
+        var result = regIsNumber(age);
+        if( age === "" || false === result ) {
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    function regIsNumber(fData)
+    {
+        var reg = new RegExp("^[-]?[0-9]+[\.]?[0-9]+$");
+        return reg.test(fData)
+    }
+
+
+    function getSexAgeRegion() {
+        var sexAgeRegion = {};
+        var sexAge = $(".sexAge");
+        sexAgeRegion.sex = sexAge.find("input[name = 'sex'][checked]").val();
+        sexAgeRegion.age = sexAge.find("input[type = 'text']").val();
+        sexAgeRegion.region = sexAge.find("select.form-control").val();
+        return sexAgeRegion;
+    }
+
+});
