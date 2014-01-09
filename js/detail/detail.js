906c8be6a26b40803e123425ad3a968f157f7c68
freestyle21 <675861708@qq.com>
2014-01-09 01:10:47 -0800

    login '

--- js/detail/detail.js
+++ js/detail/detail.js
@@ -1,28 +1,10 @@
 define(function(require) {
+	var $ = require('jquery');	
+	require('bootstrap');
 
-	var $ = require('jquery');
-
-
-  	// 器官区域交互
-	$("#organ").find(".item").each(function() {
-		$(this).hover(function() {
-			$(this).siblings().find(".veryhuo").stop();
-			$(this).siblings().find(".veryhuo").fadeTo("fast",0.1);
-		},
-		function() {
-			$(this).siblings().find(".veryhuo").stop();
-			$(this).siblings().find(".veryhuo").fadeTo("fast",0);
-		});
-	});
-
-  	//症状选择区域交互
-  	$('#other .title:first').addClass('selected');
-  	$('#other .title').on('click',function(e){
-  		$('#other .title').removeClass('selected')
-  		$('#other .content').slideUp();
-  		$(e.target).addClass('selected');
-  		$(e.target).next('.content').slideDown();
-
-  	})
+	var detail = require('detail/detail-select');
+	detail.organRes();
+	detail.otherRes();
+	detail.resultRes();
 });
 
